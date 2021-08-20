81a9e6a847cf5fd34ed40dfb248994cad4ef492a
Igor Mukhametdinov <mukhametdinovigor@gmail.com>
2021-08-20 22:54:11 +1100

    Add two models

--- test_app/models.py
+++ test_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField('Имя', max_length=100)
+    last_name = models.CharField('Фамилий', max_length=100)
+    address = models.CharField('Адрес', max_length=100)
+
+
+class Post(models.Model):
+    title = models.CharField('Название', max_length=100)
+    text = models.TextField('Текст')
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, verbose_name='Автор')
