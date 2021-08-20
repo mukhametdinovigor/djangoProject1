b89c18d63fbb09965a513fc8263e49b60c26f882
Igor Mukhametdinov <mukhametdinovigor@gmail.com>
2021-08-20 22:44:45 +1100

    Test

--- djangoProject1/urls.py
+++ djangoProject1/urls.py
@@ -1,5 +1,7 @@
 """djangoProject1 URL Configuration
 
+
+
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
